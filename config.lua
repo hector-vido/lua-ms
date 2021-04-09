42771a85712b62bd953fd4b6ab1001505a4ceebd
Hector Vido <hector_vido@yahoo.com.br>
2021-04-09 18:10:47 -0300

    Manifestos e alteração em nginx.conf para k8s

--- config.lua
+++ config.lua
@@ -2,12 +2,12 @@
 local config = require('lapis.config')
 
 config('development', {
-  secret = 'UxNGNjv5qjbmhpbRjTnVNGknUEzBQnee',
+  secret = os.getenv('SECRET') or 'UxNGNjv5qjbmhpbRjTnVNGknUEzBQnee',
   mysql = {
-    host = '172.17.0.1',
-    port = '3306',
-    user = 'lua',
-    password = '4linux',
-    database = 'lua'
+    host = os.getenv('DB_HOST') or '172.17.0.1',
+    port = os.getenv('DB_PORT') or '3306',
+    user = os.getenv('DB_USER') or 'lua',
+    password = os.getenv('DB_PASSWORD') or '4linux',
+    database = os.getenv('DB_DATABASE') or 'lua'
   }
 })
